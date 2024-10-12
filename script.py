9134fc792a424170cf96582f15e54b1869b29064
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-10-12 18:52:43 -0300

    Update script.py

--- script.py
+++ script.py
@@ -20,6 +20,8 @@ cAlteraRegistro -> Verifica se será feita uma atualização em algum dos
                    dados de um dos produtos                                 (Caractere)
 cChaveProduto   -> Recebe a chave do produto que será alterado na função
                    'alterarProdutos'                                        (Caractere)
+cCampoAlterado  -> Informa qual campo da tabela de registro do produto deve
+                   ser alterado                                             (Caractere)
 '''
 
 dictProduto = {}
@@ -30,15 +32,17 @@ def menuPrincipal():
     print('Menu principal do cadastro de produto:\n'
         '1 - Cadastrar novo produto\n'
         '2 - Consultar dados de produto\n'
-        '3 - Excluir produto cadastrado\n'
-        '4 - Sair do programa')
+        '3 - Alterar dados de produto\n'
+        '4 - Excluir produto cadastrado\n'
+        '5 - Sair do programa')
     try:
         nOpcaoMenu = int(input('Selecione qual opção deseja seguir: '))
         match nOpcaoMenu:
             case 1: cadastraProduto()
             case 2: consultaCadastro()
-            case 3: excluiProduto()
-            case 4: sairPrograma()
+            case 3: alterarRegistros()
+            case 4: excluiProduto()
+            case 5: sairPrograma()
     except ValueError:
         print('O valor digitado deve estar entre as opções listadas.')
         time.sleep(2)
@@ -65,11 +69,10 @@ def cadastraProduto():
             
 
 def enviaDadosProduto(nCodProduto, cNomeProduto, cUnidadeMedida, fPrecoPadrao, fDescontoMax):
-    dictSuporte = {nCodProduto: {   'Nome': cNomeProduto.ljust(20),
-                                    'UnidadeMedida': cUnidadeMedida.ljust(4),
-                                    'PrecoPadrao': fPrecoPadrao.ljust(5),
-                                    'DescontoMaximo': fDescontoMax.ljust(2)}}
-    # dictProduto.update(dictSuporte)
+    dictSuporte = {nCodProduto: {   'Nome': cNomeProduto.ljust(15),
+                                    'UnidadeMedida': cUnidadeMedida.ljust(15),
+                                    'PrecoPadrao': fPrecoPadrao.ljust(15),
+                                    'DescontoMaximo': fDescontoMax.ljust(15)}}
     connection.create(nCodProduto, cNomeProduto, cUnidadeMedida, float(fPrecoPadrao), int(fDescontoMax))
     print(f'Cadastro realizado para o produto com código {nCodProduto}\n{dictSuporte[nCodProduto]}')
 
@@ -77,17 +80,28 @@ def enviaDadosProduto(nCodProduto, cNomeProduto, cUnidadeMedida, fPrecoPadrao, f
 def consultaCadastro():
     os.system('cls')
     print('Bem vindo à tela de consulta dos produtos!\nOs seguintes produtos estão cadastrados:')
-    for chave in dictProduto.keys():
-        print(f'{chave}: {dictProduto[chave]}')
+    connection.select()
+    input('Pressione Enter para voltar ao menu principal')
+    menuPrincipal()
 
+#=-=-=-=-=-=-=-=-=-=ALTERAÇÃO DE REGISTRO=-=-=-=-=-=-=-=-=-=#
 def alterarRegistros():
+    os.system('cls')
+    connection.select()
     cChaveProduto = input('Insira o código do produto que deverá ser alterado?')
-    print(f'O produto está com os seguintes dados cadastrados:\n{dictProduto[cChaveProduto]}\nInsira os novos valores:')
-    cNomeProduto    = input('Nome: ')
-    cUnidadeMedida  = input('Unidade de medida: ')
-    fPrecoPadrao    = input('Preço padrão: ')
-    fDescontoMax    = input('Desconto máximo: ')
-    enviaDadosProduto(cChaveProduto, cNomeProduto, cUnidadeMedida, fPrecoPadrao, fDescontoMax)
+    print(f'O produto está com os seguintes dados cadastrados:')
+    connection.select(cChaveProduto)
+    cCampoAlterado = input('Qual campo deverá ser alterado (NOME, UNIMEDIDA, PRECOPAD, DESCONTOMAX)?')
+    if cCampoAlterado.upper().strip(' ') not in ['NOME', 'UNIMEDIDA', 'PRECOPAD', 'DESCONTOMAX']:
+        print('Este não é um campo disponível para alteração! Por favor, selecione outro.')
+        time.sleep(3)
+        alterarRegistros()
+    cNovoValor = input(f'Insira o novo valor para o campo {cCampoAlterado}:\nDigite "sair" para cancelar.')
+    if cNovoValor.upper().strip(' ') == 'SAIR':
+        print('Processo de alteração cancelado! Voltando ao menu principal...')
+        time.sleep(3)
+        menuPrincipal()
+    connection.update(cChaveProduto,cCampoAlterado,cNovoValor)
     print('Produto alterado!')
     input('Pressione uma tecla para seguir:')
     menuPrincipal()
@@ -96,18 +110,15 @@ def alterarRegistros():
 def excluiProduto():
     os.system('cls')
     print('Bem vindo à tela de exclusão de produtos!\n Os seguintes produtos estão cadastrados:')
-    for chave in dictProduto.keys():
-        print(f'{chave}: {dictProduto[chave]}')
+    connection.select()
     removeProduto = input('Digite o código do produto que deverá ser excluído: ')
-    print(dictProduto[removeProduto])
-    dictProduto.pop(removeProduto)
-    print('Produto removido! Pressione uma tecla para seguir:')
+    connection.delete(removeProduto)
+    input('Produto removido! Pressione Enter para seguir:')
     menuPrincipal()
 
-#=-=-=-=-=-=-=-=-=-=EXCLUSÃO DE PRODUTOS=-=-=-=-=-=-=-=-=-=#
+#=-=-=-=-=-=-=-=-=-=SAIR DO SISTEMA=-=-=-=-=-=-=-=-=-=#
 def sairPrograma():
     os.system('cls')
     print('Programa encerrado!')
 
 menuPrincipal()
-
